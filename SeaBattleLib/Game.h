d08d8bbde047175ed936e2387f75fea80957737c
IgorVif <robbonn001@gmail.com>
2026-09-24 02:21:17 +0300

    добавлено объявление классов Player, Game

--- SeaBattleLib/Game.h
+++ SeaBattleLib/Game.h
@@ -1,5 +1,13 @@
 #pragma once
+#include "Player.h"
+#include <ctime>   // для time()
+
 class Game
 {
+	Player player1, player2;
+public:
+	Game();
+	void start();
+	void win(const Player& winner) const noexcept;
 };
 
